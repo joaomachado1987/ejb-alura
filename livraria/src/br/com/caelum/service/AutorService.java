91777f4d8e4f5e77dcd01267d43bed7e5e81d54e
joaozinhogaucho <joaozinhogaucho@gmail.com>
2017-08-29 21:44:21 -0300

    f

---
+++ livraria/src/br/com/caelum/service/AutorService.java
@@ -0,0 +1,24 @@
+package br.com.caelum.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.caelum.livraria.dao.AutorDao;
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorService {
+	
+	@Inject
+	private AutorDao autorDao;
+	
+	public void salva(Autor autor) {
+		this.autorDao.salva(autor);		
+	}
+
+	public List<Autor> todosAutores() {		
+		return this.autorDao.todosAutores();
+	}
+}
