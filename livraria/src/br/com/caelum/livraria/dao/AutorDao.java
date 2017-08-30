302fd1948a6fa13db60e0e7d0e88785b5139a3f9
joaozinhogaucho <joaozinhogaucho@gmail.com>
2017-08-29 21:44:03 -0300

    comiit

--- livraria/src/br/com/caelum/livraria/dao/AutorDao.java
+++ livraria/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -3,19 +3,34 @@ package br.com.caelum.livraria.dao;
 import java.util.List;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Autor;
 
 @Stateless
+//@TransactionManagement(TransactionManagementType.BEAN)
 public class AutorDao {
 
+//	@Inject
+//	private UserTransaction tx;
+	
 	@PersistenceContext
 	private EntityManager entityManager;
 	
+	@TransactionAttribute(TransactionAttributeType.MANDATORY)
 	public void salva(Autor autor) {
-		entityManager.persist(autor);
+//		try {
+//			tx.begin();
+			entityManager.persist(autor);
+//			tx.commit();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
+		
 	}
 	
 	public List<Autor> todosAutores() {
--- livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
+++ livraria/src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -1,17 +1,23 @@
 package br.com.caelum.livraria.dao;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import br.com.caelum.livraria.modelo.Usuario;
 
 @Stateless	
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public class UsuarioDao {
 
 	@PersistenceContext
     private EntityManager manager;
 	
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
 	public Usuario buscaPeloLogin(String login) {
         // return this.banco.buscaPeloNome(login);
 
